133c017ff7ebbc39c87bf39b2560cee5db8bfed3
Iris153 <71736194+Iris153@users.noreply.github.com>
2020-11-29 11:18:54 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,15 @@
+package colasdinamicas;
+
+
+public class Nodo {
+    String elemento;
+    Nodo siguiente;
+    
+    public Nodo(String nombre){
+        this.elemento=nombre;
+        this.siguiente = null;
+    }
+    public Nodo(){
+        
+    }
+}
